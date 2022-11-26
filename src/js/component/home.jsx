d6eeb6515d0b6b0307d2538d0e69460087d09c95
pzagmel <pedro.zagalm@gmail.com>
2022-11-26 00:45:26 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,41 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
-  const [tareas, setTareas] = useState([
-    "Make the bed",
-    "Eat",
-    "Walk the dog",
-  ]);
+  const [tareas, setTareas] = useState([]);
+  
+  useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/pzeta")
+      .then((response) => response.json())
+      .then((data) => setTareas(data));
+  }, []);
+
+  const agregarTarea=(tarea)=>{
+    setTareas([...tareas, tarea]);
+
+  }
+  
   return (
     <div className="container">
       <h1> Todo List</h1>
       <ul>
         <li>
-      <input
-        placeholder="What needs to be done"
-        onKeyPress={(e) => {
-          if (e.key === "Enter") {
-            setTareas([...tareas, e.target.value]);
-            e.target.value = "";
-          }
-        }}
-      />
-</li>
-      {tareas.map((value, index) => {
-        return (
-          <li key={index}>
-            {value}
-            <i
-              className="fas fa-times float-end my-1 mx-1"
-              onClick={() =>
-                setTareas(tareas.filter((value, i) => index != i))
+          <input
+            placeholder="What needs to be done"
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                agregarTarea( {label:e.target.value, done:true})
+                e.target.value = "";
               }
-            ></i>
-          </li>
-        );
-    })}
-    <div className ="left"> {tareas.length} Item left </div>
+            }}
+          />
+        </li>
+        {tareas.map((value, index) => {
+          return (
+            <li key={index}>
+              {value.label}
+              <i
+                className="fas fa-times float-end my-1 mx-1"
+                onClick={() =>
+                  setTareas(tareas.filter((value, i) => index != i))
+                }
+              ></i>
+            </li>
+          );
+        })}
+        <div className="left"> {tareas.length} Item left </div>
       </ul>
     </div>
   );
